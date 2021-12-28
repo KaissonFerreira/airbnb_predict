02406bfa7019335549c7ea128fea66ea239523a4
KaissonFerreira <kaysson_17@hotmail.com>
2021-12-27 22:17:01 -0300

    Primeiro ciclo

---
+++ config.py
@@ -0,0 +1,8 @@
+# PATH
+TRAIN_PATH = 'D:\CDS\Projetos\PA01\airbnb_predict\Source\train_users_2.csv'
+TEST_PATH = 'D:\CDS\Projetos\PA01\airbnb_predict\Source\test_users.csv'
+SESSIONS_PATH = 'D:\CDS\Projetos\PA01\airbnb_predict\Source\sessions.csv'
+AGE_PATH ='D:\CDS\Projetos\PA01\airbnb_predict\Source\age_gender_bkts.csv'
+COUNTRIES = 'D:\CDS\Projetos\PA01\airbnb_predict\Source\countries.csv'
+SAMPLE = 'D:\CDS\Projetos\PA01\airbnb_predict\Source\sample_submission_NDF.csv'
+
